f192e8fe342f10b1b32e5612c99d4273a9511dde
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-08-09 16:38:11 -0700

    Section 10: Managing State & Data with Stores

---
+++ s10/src/Products/Products.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { products } from "./products-store";
+  import Product from "../Products/Product.svelte";
+
+  // let products = [
+  //   {
+  //     id: "p1",
+  //     title: "A Book",
+  //     price: 9.99,
+  //     description: "A great book!"
+  //   },
+  //   {
+  //     id: "p2",
+  //     title: "A Carpet",
+  //     price: 99.99,
+  //     description: "Red and green."
+  //   }
+  // ];
+</script>
+
+<section>
+  <h1>Products</h1>
+  {#each $products as product (product.id)}
+    <Product
+      id={product.id}
+      title={product.title}
+      price={product.price}
+      description={product.description}
+    />
+  {/each}
+</section>
+
+<style>
+  section {
+    width: 30rem;
+    max-width: 90%;
+    margin: 2rem auto;
+  }
+</style>
